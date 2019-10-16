d12c9688d614b0471c44654f94fc3aedeff90342
lamhung <lamhungypl@gmail.com>
2019-10-16 13:57:25 +0700

    stater medium todolist

--- App.js
+++ App.js
@@ -1,35 +1,37 @@
-import React, {useEffect} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-import TodoApp from './src/TodoApp';
-import store from './src/store';
-import {Provider} from 'react-redux';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, Button} from 'react-native';
 
-const App = () => {
-  // useEffect(() => {
-  //   console.log('store', store.getState());
-  // });
+export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <>
-      <Provider store={store}>
-        <TodoApp />
-      </Provider>
-    </>
+    <View style={styles.container}>
+      <Text>You clicked {count} times.</Text>
+      <Button
+        onPress={() => setCount(count + 1)}
+        title="Click me"
+        color="red"
+        accessibilityLabel="Click this button to increase count"
+      />
+    </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
   },
 });
-
-export default App;
--- src/actions/index.js
+++
@@ -1,8 +0,0 @@
-import {ADD_TODO, TOGGLE_TODO} from './actionTypes';
-let nextId = 0;
-export const addTodo = text => {
-  return {type: ADD_TODO, id: nextId++, text};
-};
-export const toggleTodo = id => {
-  return {type: TOGGLE_TODO, id};
-};
